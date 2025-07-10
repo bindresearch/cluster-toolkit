9d59e6070e786a688d3df1796dccd2e8b0e41ca1
Candide Champion <candide.champion@bindresearch.org>
2025-07-10 11:57:10 +0100

    minor modifs to the VM creation

--- initialize_instance/create_gcloud_vm_instance.sh
+++ initialize_instance/create_gcloud_vm_instance.sh
@@ -2,12 +2,18 @@
 
 # Create a basic ubuntu 2024 instance
 
-INSTANCE_NAME="my-ubuntu-test-instance-3"
+INSTANCE_NAME="benchmark-gromacs-gpu-docker"
+
+PROJECT_ID="esoteric-kiln-463912-m2"
+ZONE="europe-west1-b"
+
+CPU_MACHINE="n1-standard-8" # compare with n1-standard-2 and n1-standard-4
+GPU_TYPE="nvidia-tesla-t4" # compare to "nvidia-l4" (works with g2 CPUs),  "nvidia-tesla-p4" or "nvidia-tesla-p100"
 
 gcloud compute instances create ${INSTANCE_NAME} \
-    --project=esoteric-kiln-463912-m2 \
-    --zone=europe-west1-b \
-    --machine-type=n1-standard-1 \
+    --project=${PROJECT_ID} \
+    --zone=${ZONE} \
+    --machine-type=${CPU_MACHINE} \
     --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default \
     --metadata=enable-osconfig=TRUE \
     --metadata-from-file startup-script=startup.sh \
@@ -15,9 +21,9 @@ gcloud compute instances create ${INSTANCE_NAME} \
     --provisioning-model=STANDARD \
     --service-account=891723074586-compute@developer.gserviceaccount.com \
     --scopes=https://www.googleapis.com/auth/cloud-platform \
-    --accelerator=count=1,type=nvidia-tesla-t4 \
+    --accelerator=count=1,type=${GPU_TYPE} \
     --tags=http-server,https-server \
-    --create-disk=auto-delete=yes,boot=yes,device-name=${INSTANCE_NAME},disk-resource-policy=projects/esoteric-kiln-463912-m2/regions/europe-west1/resourcePolicies/default-schedule-1,image=projects/ubuntu-os-cloud/global/images/ubuntu-minimal-2404-noble-amd64-v20250701,mode=rw,size=200,type=pd-balanced \
+    --create-disk=auto-delete=yes,boot=yes,device-name=${INSTANCE_NAME},disk-resource-policy=projects/${PROJECT_ID}/regions/europe-west1/resourcePolicies/default-schedule-1,image=projects/ubuntu-os-cloud/global/images/ubuntu-minimal-2404-noble-amd64-v20250701,mode=rw,size=200,type=pd-balanced \
     --shielded-secure-boot \
     --shielded-vtpm \
     --shielded-integrity-monitoring \
@@ -25,17 +31,10 @@ gcloud compute instances create ${INSTANCE_NAME} \
     --reservation-affinity=any
 
 
-printf 'agentsRule:\n  packageState: installed\n  version: latest\ninstanceFilter:\n  inclusionLabels:\n  - labels:\n      goog-ops-agent-policy: v2-x86-template-1-4-0\n' > config.yaml
+#printf 'agentsRule:\n  packageState: installed\n  version: latest\ninstanceFilter:\n  inclusionLabels:\n  - labels:\n      goog-ops-agent-policy: v2-x86-template-1-4-0\n' > config.yaml
 
-
-
-gcloud compute instances ops-agents policies create goog-ops-agent-v2-x86-template-1-4-0-europe-west1-b \
-    --project=esoteric-kiln-463912-m2 \
-    --zone=europe-west1-b \
-    --file=config.yaml
-
-
-#
-# Add some default scripts to apt-get update and install basic drivers + cuda container toolkit.
-# 
+#gcloud compute instances ops-agents policies create goog-ops-agent-v2-x86-template-1-4-0-europe-west1-b \
+#    --project=${PROJECT_ID} \
+#    --zone=${ZONE} \
+#    --file=config.yaml
 
