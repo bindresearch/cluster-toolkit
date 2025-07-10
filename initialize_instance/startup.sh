bc8e6a0f35f73dc68ea576cae6e86740602fd09b
Candide Champion <candide.champion@bindresearch.org>
2025-07-10 10:32:19 +0100

    fix startup startup script

--- initialize_instance/startup.sh
+++ initialize_instance/startup.sh
@@ -6,9 +6,13 @@ if [ -f $checkfile ]; then
 	exit
 fi
 
-# If the file doesn't exist, run the code
+# this is to avoid the startup script running twice
+sudo touch .startup-has-executed 
 
-sudo touch .startup-has-executed
+# Put the script output somewhere we can go and have a look at it.
+exec > /var/log/startup-script.log 2>&1
+set -ex
+
 
 sudo apt-get update
 sudo apt-get install -y ubuntu-drivers-common
@@ -20,20 +24,22 @@ sudo apt-get install -y nvidia-driver-535
 # Docker install 
 #
 
-sudo reboot
+echo "\n\nnvidia drivers succesfully installed\n\n\n"
 
 #
 # Note, needed to reboot here as code needed to be run manually after here. (i.e. didn't work from startup script)
 # I will need to fix this later on
 # 
 
-sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-apt-cache policy docker-ce
-sudo apt-get install docker-ce
+sudo apt-cache policy docker-ce
+sudo apt-get install -y docker-ce
+
+echo "\n\ndocker succesfully installed\n\n\n"
 
 # Ways to get docker to run without pre-pending sudo require password, skipping for now 
 #todo later
@@ -57,7 +63,11 @@ export NVIDIA_CONTAINER_TOOLKIT_VERSION=1.17.8-1
       libnvidia-container1=${NVIDIA_CONTAINER_TOOLKIT_VERSION}
 
 
+echo "\n\nnvidia-container succesfully installed\n\n\n"
+
 # to test the install, you can run the following: sudo docker run --rm --runtime=nvidia --gpus all ubuntu nvidia-smi
+
+echo "\n\nstartup.sh succesfully completed."
 
 sudo reboot
 
