a6e36fa53e4bcbc7e850baff37f57445f490098c
Candide Champion <candide.champion@bindresearch.org>
2025-07-11 16:59:18 +0100

    add way to install cuda without containers

--- initialize_instance/startup.sh
+++ initialize_instance/startup.sh
@@ -66,6 +66,18 @@ export NVIDIA_CONTAINER_TOOLKIT_VERSION=1.17.8-1
       libnvidia-container-tools=${NVIDIA_CONTAINER_TOOLKIT_VERSION} \
       libnvidia-container1=${NVIDIA_CONTAINER_TOOLKIT_VERSION}
 
+#
+# note, if running without a container, install cuda with:
+#
+
+# wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
+# sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
+# wget https://developer.download.nvidia.com/compute/cuda/12.9.1/local_installers/cuda-repo-ubuntu2004-12-9-local_12.9.1-575.57.08-1_amd64.deb
+# sudo dpkg -i cuda-repo-ubuntu2004-12-9-local_12.9.1-575.57.08-1_amd64.deb
+# sudo cp /var/cuda-repo-ubuntu2004-12-9-local/cuda-*-keyring.gpg /usr/share/keyrings/
+# sudo apt-get update
+# sudo apt-get -y install cuda-toolkit-12-9
+
 
 echo "\n\nnvidia-container succesfully installed\n\n\n"
 
