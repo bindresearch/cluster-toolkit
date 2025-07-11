76e9ddc9c79620c517e0a2029049e11b8cc18e0f
Candide Champion <candide.champion@bindresearch.org>
2025-07-11 14:49:39 +0100

    minor automation improvement for selection

--- initialize_instance/create_gcloud_vm_instance.sh
+++ initialize_instance/create_gcloud_vm_instance.sh
@@ -5,10 +5,14 @@
 INSTANCE_NAME="benchmark-gromacs-gpu-docker"
 
 PROJECT_ID="esoteric-kiln-463912-m2"
-ZONE="europe-west1-b"
 
-CPU_MACHINE="n1-standard-8" # compare with n1-standard-2 and n1-standard-4
+AREA="europe-west1"
+ZONE="${AREA}-b"
+
+CPU_MACHINE="n1-standard-1" # compare with n1-standard-2 and n1-standard-4
 GPU_TYPE="nvidia-tesla-t4" # compare to "nvidia-l4" (works with g2 CPUs),  "nvidia-tesla-p4" or "nvidia-tesla-p100"
+
+INSTANCE_NAME="${INSTANCE_NAME}-${GPU_TYPE}"
 
 gcloud compute instances create ${INSTANCE_NAME} \
     --project=${PROJECT_ID} \
@@ -23,8 +27,8 @@ gcloud compute instances create ${INSTANCE_NAME} \
     --scopes=https://www.googleapis.com/auth/cloud-platform \
     --accelerator=count=1,type=${GPU_TYPE} \
     --tags=http-server,https-server \
-    --create-disk=auto-delete=yes,boot=yes,device-name=${INSTANCE_NAME},disk-resource-policy=projects/${PROJECT_ID}/regions/europe-west1/resourcePolicies/default-schedule-1,image=projects/ubuntu-os-cloud/global/images/ubuntu-minimal-2404-noble-amd64-v20250701,mode=rw,size=200,type=pd-balanced \
-    --shielded-secure-boot \
+    --create-disk=auto-delete=yes,boot=yes,device-name=${INSTANCE_NAME},image=projects/ubuntu-os-cloud/global/images/ubuntu-minimal-2404-noble-amd64-v20250701,mode=rw,size=200,type=pd-balanced \
+    --no-shielded-secure-boot \
     --shielded-vtpm \
     --shielded-integrity-monitoring \
     --labels=goog-ops-agent-policy=v2-x86-template-1-4-0,goog-ec-src=vm_add-gcloud \
